3dc53bb8c7dc94ad8578c744314d90bacfce0066
fakherddine <fakherddine.ab.hamda@gmail.com>
2021-03-09 22:56:36 +0200

    step5

---
+++ javascript-basics-01/step5.js
@@ -0,0 +1,6 @@
+function multi(){
+    var a=parseInt(document.getElementById("first_number").value);
+    var b=parseInt(document.getElementById("second_number").value);
+    var res=a*b;
+    alert(a+" x "+b+" = "+res);
+}
