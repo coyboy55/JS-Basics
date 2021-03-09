c56b0b928d0f306869b054b04039db077f0049c6
fakherddine <fakherddine.ab.hamda@gmail.com>
2021-03-09 23:02:06 +0200

    step6

---
+++ javascript-basics-01/step6.js
@@ -0,0 +1,6 @@
+function div(){
+    var a=parseInt(document.getElementById("first_number").value);
+    var b=parseInt(document.getElementById("second_number").value);
+    var res=parseFloat(a/b);
+    alert(a+" / "+b+" = "+res);
+}
