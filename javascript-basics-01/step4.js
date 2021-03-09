27142816d0382554704e690c26ce42c3b77deaae
fakherddine <fakherddine.ab.hamda@gmail.com>
2021-03-09 22:43:54 +0200

    step4

---
+++ javascript-basics-01/step4.js
@@ -0,0 +1,6 @@
+function val(){
+    var name=document.getElementById("name").value;
+    var surname=document.getElementById("surname").value;
+    var city=document.getElementById("city").value;
+alert("nom: "+name+"\n"+"prenom: "+surname+"\n"+"ville: "+city);
+}
