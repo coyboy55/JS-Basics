f4d2381510ff1c2f2035aed14499b79a06229d57
fakherddine <fakherddine.ab.hamda@gmail.com>
2021-03-09 23:17:00 +0200

    step8

---
+++ javascript-basics-01/step8.js
@@ -0,0 +1,9 @@
+function verif(){
+    var a=parseInt(document.getElementById("age").value);
+    if (a<18){
+        alert("you are under 18");
+        
+    }else{
+        alert("you are over 18")
+    }
+}
