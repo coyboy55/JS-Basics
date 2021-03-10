76a0a4db70582302d663be2bd009a0be73d7df5d
fakherddine <fakherddine.ab.hamda@gmail.com>
2021-03-10 11:34:52 +0200

    step4

---
+++ javascript-basics-02/step 4/step4.js
@@ -0,0 +1,9 @@
+function res(){
+    if(confirm("are u sure to reset")){
+     document.getElementById("name").value="";
+     document.getElementById("surname").value="";
+
+     document.getElementById("city").value="";
+
+    }
+}
