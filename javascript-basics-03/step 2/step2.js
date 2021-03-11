7e44d61071e383d53fc2bd0bfb96d352596db3a3
fakherddine <fakherddine.ab.hamda@gmail.com>
2021-03-11 18:36:48 +0200

    step2

---
+++ javascript-basics-03/step 2/step2.js
@@ -0,0 +1,8 @@
+
+    document.getElementById("show").addEventListener("click",function(){document.getElementById("texte").style.display="block";});
+    
+    
+    document.getElementById("hide").addEventListener("click",function(){document.getElementById("texte").style.display="none";});
+    
+    
+
