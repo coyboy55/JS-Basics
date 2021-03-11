d5e390a9a0d48f4f06f9f269dbc57be1743c649b
fakherddine <fakherddine.ab.hamda@gmail.com>
2021-03-11 18:56:19 +0200

    step3

---
+++ javascript-basics-03/step 3/step3.js
@@ -0,0 +1,9 @@
+function gre(){
+    document.getElementById("text").style.color="green";
+}
+function red(){
+    document.getElementById("text").style.color="red";
+}
+function blu(){
+    document.getElementById("text").style.color="blue";
+}
